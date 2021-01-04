e14751e3163f0108ea71b3ce92a5050ad7db2fab
kenken <kajikagarden.2020@gmail.com>
2021-01-04 20:16:15 +0900

    add compass

--- 249.js
+++ 249.js
@@ -27,7 +27,7 @@ if (window.DeviceOrientationEvent) {
 // ここまでiOS13対応の分岐コード
 
 // DeviceOrientationEvent
-window.addEventListener('deviceorientation', deviceorientationHandler);
+// window.addEventListener('deviceorientation', deviceorientationHandler);
 
 // ジャイロセンサーの値が変化するとその値を取得する
 // 各軸にのデータをeventから取得し、定数へ代入
@@ -47,7 +47,42 @@ function deviceorientationHandler(event) {
   console.log(beta);
   console.log(gamma);
   console.log(alpha);
+
+  // 引数に取得した３軸ジャイロ値alpha, beta, gammaをとる
+  // getCompassHeading関数を定義
+  function getCompassHeading(alpha, beta, gamma) {
+    // 角度をラジアン変換
+    const degtorad = Math.PI / 180;
+
+    const _x = beta ? beta * degtorad : 0;
+    const _y = gamma ? gamma * degtorad : 0;
+    const _z = alpha ? alpha * degtorad : 0;
+
+    const cY = Math.cos(_y);
+    const cZ = Math.cos(_y);
+    const sX = Math.sin(_x);
+    const sY = Math.sin(_y);
+    const sZ = Math.sin(_z);
+
+    const Vx = -cZ * sY - sZ * sX * cY;
+    const Vy = -sZ * sY + cZ * sX * cY;
+
+    let compassHeading = Math.atan(Vx / Vy);
+
+    if (Vy < 0) {
+      compassHeading += Math.PI;
+    } else if (Vx < 0) {
+      compassHeading += 2 * Math.PI;
+    }
+
+    return compassHeading * (180 / Math.PI);
+    console.log(compassHeading);
+  }
 }
+
+
+// ここからコンパス描画
+
 
 
 // function getCompassHeading(alpha, beta, gamma) {
